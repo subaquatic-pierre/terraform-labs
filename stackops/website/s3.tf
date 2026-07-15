ba355ce173b0b18cf5750e31ab7b714ed64fd403
Pierre du Toit <subaquatic.pierre@gmail.com>
2026-07-15 15:21:47 +0400

    create website module

--- stackops/website/s3.tf
+++ stackops/website/s3.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "stackops" {
-  bucket = "${var.project_name}-bucket"
+  bucket        = "${var.project_name}-bucket"
+  force_destroy = true
 
   tags = {
     Name = "${var.project_name}-bucket"
