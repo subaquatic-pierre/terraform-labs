de5f50cfa28a68e178bc15aac40260ea628ed6c6
Pierre du Toit <subaquatic.pierre@gmail.com>
2026-07-17 13:50:07 +0400

    remove unused folders in eks

--- static-website/scripts/deploy-static.sh
+++ static-website/scripts/deploy-static.sh
@@ -6,7 +6,7 @@ DISTRIBUTION_ID=$(terraform output -raw cloudfront_id)
 
 echo "Deploying to bucket: ${BUCKET}"
 
-aws s3 sync ./apps/static-website/ "s3://${BUCKET}" --delete
+aws s3 sync ./apps/example-website/ "s3://${BUCKET}" --delete
 
 echo "Creating CloudFront invalidation: ${DISTRIBUTION_ID}"
 
