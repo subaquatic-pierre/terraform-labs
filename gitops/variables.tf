e35195f71679fdf9b31443d7ce0c26c1302f02c4
Pierre du Toit <subaquatic.pierre@gmail.com>
2026-07-17 13:38:16 +0400

    update eks module

---
+++ gitops/variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  default = "eu-west-1"
+}
+
+
+variable "bucket_name_prefix" {
+  type = string
+}
+
+variable "aws_access_key" {
+  type      = string
+  sensitive = true
+}
+
+
+variable "aws_secret_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "Cluster Name"
+  type        = string
+}
